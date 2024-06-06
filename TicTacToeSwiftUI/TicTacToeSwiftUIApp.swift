ea2608d04319c2c5cf26194da4053b206fb91392
Mohsporty <m.alassdi@gmail.com>
2024-06-06 18:12:52 +0300

    Initial Commit

---
+++ TicTacToeSwiftUI/TicTacToeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftUIApp.swift
+//  TicTacToeSwiftUI
+//
+//  Created by Mohammad  on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

77231d883c97584d37f4c9a914f4c96fd02f0b64
Mohsporty <m.alassdi@gmail.com>
2024-06-08 18:41:54 +0300

    totolay update plases check all between swift and swift ui

--- TicTacToeSwiftUI/Cell.swift
+++ TicTacToeSwiftUI/Cell.swift
@@ -1,8 +1,39 @@
-//
-//  Cell.swift
-//  TicTacToeSwiftUI
-//
-//  Created by Mohammad  on 08/06/2024.
-//
 
 import Foundation
+import SwiftUI
+
+struct Cell
+{
+    var tile : Tile
+    func displayTile() -> String 
+    {
+        switch(tile)
+        {
+        case Tile.Nought :
+            return "O"
+        case Tile.Cross :
+            return "X"
+        default :
+            return ""
+        }
+    }
+    func tileColor() -> Color
+    {
+        switch(tile)
+        {
+        case Tile.Nought :
+            return Color.red
+        case Tile.Cross :
+            return Color.black
+        default :
+            return Color.black
+        }
+    }
+}
+
+enum Tile
+{
+    case Nought
+    case Cross
+    case Empty
+}

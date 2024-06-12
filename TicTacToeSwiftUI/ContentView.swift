6610c5d9b81036934bf7988c9a3ead54c32dc4d3
Mohsporty <m.alassdi@gmail.com>
2024-06-12 17:40:49 +0300

    fixed the bugs and wear work in font

--- TicTacToeSwiftUI/ContentView.swift
+++ TicTacToeSwiftUI/ContentView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var  gameState = GameState()
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        let boarderSize = CGFloat(5)
+        VStack(spacing: boarderSize) {
+            ForEach (0...2, id: \.self) {
+                row in
+                HStack(spacing: boarderSize)
+                {
+                    ForEach(0...2, id: \.self)
+                    {
+                        column in
+                        
+                        let cell = gameState.board[row][column]
+                        
+                        
+                        Text(cell.displayTile())
+                            .font(.system(size: 60))
+                            .foregroundColor(cell.tileColor())
+                            .bold()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.white)
+                            .onTapGesture{
+                                gameState.placeTile(row, column: column)
+                            }
+                    }
+                }
+            }
         }
+        .background(Color.black)
         .padding()
     }
 }
--- TicTacToeSwiftUI/GameState.swift
+++ TicTacToeSwiftUI/GameState.swift
@@ -1,8 +1,37 @@
-//
-//  GameState.swift
-//  TicTacToeSwiftUI
-//
-//  Created by Mohammad  on 08/06/2024.
-//
-
 import Foundation
+
+
+class GameState : ObservableObject
+{
+    @Published var board = [[Cell]]()
+    @Published var turn = Tile.Cross
+    
+    init()
+    {
+        resetBoard()
+    }
+    func  placeTile(_ row: Int, column: Int) {
+        if (board[row][column].tile != Tile.Empty){
+            return
+        }
+        
+        board[row][column].tile = turn == Tile.Cross ? Tile.Cross : Tile.Nought
+        
+    }
+        func resetBoard() {
+            
+            var newBoard =  [[Cell]]()
+            
+            for _ in 0...2
+            {
+                var row = [Cell]()
+                for _ in 0...2
+                {
+                    row.append(Cell(tile: Tile.Empty))
+                }
+                newBoard.append(row)
+            }
+            board = newBoard
+        }
+    }
+

41b78cb863eec4e1f821afb2d154464a26ee73fa
Mohsporty <m.alassdi@gmail.com>
2024-06-15 03:11:06 +0300

    fixed bugs and know start bulding the scorer titile in the project in (contentview )

--- TicTacToeSwiftUI/GameState.swift
+++ TicTacToeSwiftUI/GameState.swift
@@ -1,96 +1,102 @@
 import Foundation
 
-
-class GameState : ObservableObject
-{
+class GameState: ObservableObject {
     @Published var board = [[Cell]]()
     @Published var turn = Tile.Cross
     @Published var noughtsScore = 0
     @Published var crossesScore = 0
     @Published var showAlert = false
     @Published var alertMessage = "Draw"
-    init()
-    {
+
+    init() {
         resetBoard()
     }
-    func  placeTile(_ row: Int, column: Int) {
-        if (board[row][column].tile != Tile.Empty){
+
+    func placeTile(_ row: Int, column: Int) {
+        if (board[row][column].tile != Tile.Empty) {
             return
         }
-   
+
         board[row][column].tile = turn == Tile.Cross ? Tile.Cross : Tile.Nought
-      
-        
-        if(checkForVictory())
-        {
-            if(turn == Tile.Cross) {
+
+        if (checkForVictory()) {
+            if (turn == Tile.Cross) {
                 crossesScore += 1
-                
-            }
-            else {
+            } else {
                 noughtsScore += 1
             }
             let winner = turn == Tile.Cross ? "Crosses" : "Noughts"
-            alertMessage = winner + "Win!"
+            alertMessage = winner + " Win!"
             showAlert = true
-        }
-        else {
-      
+        } else {
             turn = turn == Tile.Cross ? Tile.Nought : Tile.Cross
         }
-        
-        func checkForVictory() -> Bool {
-            // vertical victoriys
-                
-                if isTurnTitle(0,  0) && isTurnTitle(1, 0) && isTurnTitle(2, 0){
-                    return true
-                }
-                if isTurnTitle(0,  1) && isTurnTitle(1, 1) && isTurnTitle(2, 1){
-                    return true
-                }
-                if isTurnTitle(0,  2) && isTurnTitle(1, 2) && isTurnTitle(2, 2){
-                    return true
-                }  // Horizantel  victoriys
-            
-            if isTurnTitle(0,  0) && isTurnTitle(0, 1) && isTurnTitle(0, 2){
-                return true
-            }
-            if isTurnTitle(0,  0) && isTurnTitle(0, 1) && isTurnTitle(1, 2){
-                return true
-            }
-            if isTurnTitle(0,  2) && isTurnTitle(1, 2) && isTurnTitle(2, 2){
-                return true
-            }
-            // dignetcch   victoriys
-        
-        if isTurnTitle(0,  0) && isTurnTitle(1, 1) && isTurnTitle(2, 2){
-            return true
-        }
-        if isTurnTitle(0,  2) && isTurnTitle(1, 1) && isTurnTitle(2, 0){
-            return true
-        }
-            return false
-        }
-    }
-    
-    func isTurnTitle(_ row: Int,_ column: Int) -> Bool
-    {
-        return board [row][column].tile == turn
-    }
-        func resetBoard() {
-            
-            var newBoard =  [[Cell]]()
-            
-            for _ in 0...2
-            {
-                var row = [Cell]()
-                for _ in 0...2
-                {
-                    row.append(Cell(tile: Tile.Empty))
-                }
-                newBoard.append(row)
-            }
-            board = newBoard
+
+        if (checkForDraw()) {
+            alertMessage = "Draw"
+            showAlert = true
         }
     }
 
+    func checkForDraw() -> Bool {
+        for row in board {
+            for cell in row {
+                if cell.tile == Tile.Empty {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    func checkForVictory() -> Bool {
+        // Vertical victories
+        if isTurnTitle(0, 0) && isTurnTitle(1, 0) && isTurnTitle(2, 0) {
+            return true
+        }
+        if isTurnTitle(0, 1) && isTurnTitle(1, 1) && isTurnTitle(2, 1) {
+            return true
+        }
+        if isTurnTitle(0, 2) && isTurnTitle(1, 2) && isTurnTitle(2, 2) {
+            return true
+        }
+        
+        // Horizontal victories
+        if isTurnTitle(0, 0) && isTurnTitle(0, 1) && isTurnTitle(0, 2) {
+            return true
+        }
+        if isTurnTitle(1, 0) && isTurnTitle(1, 1) && isTurnTitle(1, 2) {
+            return true
+        }
+        if isTurnTitle(2, 0) && isTurnTitle(2, 1) && isTurnTitle(2, 2) {
+            return true
+        }
+        
+        // Diagonal victories
+        if isTurnTitle(0, 0) && isTurnTitle(1, 1) && isTurnTitle(2, 2) {
+            return true
+        }
+        if isTurnTitle(0, 2) && isTurnTitle(1, 1) && isTurnTitle(2, 0) {
+            return true
+        }
+        
+        return false
+    }
+
+    func isTurnTitle(_ row: Int, _ column: Int) -> Bool {
+        return board[row][column].tile == turn
+    }
+
+    func resetBoard() {
+        var newBoard = [[Cell]]()
+
+        for _ in 0...2 {
+            var row = [Cell]()
+            for _ in 0...2 {
+                row.append(Cell(tile: Tile.Empty))
+            }
+            newBoard.append(row)
+        }
+        board = newBoard
+    }
+}
